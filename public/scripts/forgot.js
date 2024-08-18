6f1201799d4caabef72ea75eb8483e9964d9f600
udit2303 <udit2303@gmail.com>
2024-08-18 17:58:17 +0530

    feat

---
+++ public/scripts/forgot.js
@@ -0,0 +1,29 @@
+document.getElementById('forgot-password-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const responseMessage = document.getElementById('response-message');
+    const loaderContainer = document.getElementById('loader-wrapper');
+    loaderContainer.classList.remove("hidden");
+    // Clear any previous message
+    responseMessage.textContent = '';
+    
+    // Simulate sending the request (here you would actually send a request to your server)
+    fetch('http://localhost:3000/auth/forgot-password',
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email })
+    })
+    .then(response => {
+        loaderContainer.classList.add("hidden");
+        responseMessage.textContent = "If an account exists, a reset password email has been sent."
+
+    })
+    .catch(error => {
+        loaderContainer.classList.add("hidden");
+        // Display the error message
+        responseMessage.textContent = "If an account exists, a reset password email has been sent."
+    });
+});
---
+++ public/scripts/reset.js
@@ -0,0 +1,51 @@
+document.getElementById('reset-password-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const password = document.getElementById('pass').value;
+    const confirmPassword = document.getElementById('confirmPass').value;
+    const responseMessage = document.getElementById('response-message');
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    
+    responseMessage.textContent = '';
+    responseMessage.style.color = 'red';
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!passwordRegex.test(password)) {
+        responseMessage.textContent = 'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.';
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        responseMessage.textContent = 'Passwords do not match!';
+        return;
+    }
+    const loaderContainer = document.getElementById('loader-wrapper');
+    loaderContainer.classList.remove("hidden");
+    fetch(`http://localhost:3000/auth/reset-password`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            token: token,
+            password: password }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        loaderContainer.classList.add("hidden");
+        if (data.success) {
+            responseMessage.textContent = 'Your password has been successfully reset.';
+            responseMessage.style.color = '#28a745';
+            setTimeout(() => {
+                window.location.href = '/login.html';
+            }, 3000);
+        } else {
+            responseMessage.textContent = 'Error resetting password. Please try again.';
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        responseMessage.textContent = 'Error resetting password. Please try again.';
+    });
+});
