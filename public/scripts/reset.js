95217a6f03aba6045fdae9099e51aa1b86c3423a
udit2303 <udit2303@gmail.com>
2024-08-18 18:04:32 +0530

    fix

--- public/scripts/reset.js
+++ public/scripts/reset.js
@@ -22,7 +22,7 @@ document.getElementById('reset-password-form').addEventListener('submit', functi
     }
     const loaderContainer = document.getElementById('loader-wrapper');
     loaderContainer.classList.remove("hidden");
-    fetch(`https://yatramitra-backend.onrender.com//auth/reset-password`, {
+    fetch(`https://yatramitra-backend.onrender.com/auth/reset-password`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
